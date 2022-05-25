63d858d224a6b1f288bf2aeaefeda4a1e92f676c
piyushtsr <piyushrahangdale21@gmail.com>
2022-05-25 10:51:15 +0530

    basic integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import './index.css';
 
-function App() {
+const alanKey = 'b49f0d74731e75fc94d208a8043361b02e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+export const App = () => {
+  
+  useEffect(()=>{
+    alanBtn({
+      key: alanKey,
+      onCommand:({command,articles}) => {
+        if(command==='newHeadlines'){
+          console.log(articles);
+        }
+      }
+    })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1> Alan AI News APP </h1>
     </div>
-  );
+  )
 }
+
 
 export default App;
